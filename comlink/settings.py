8245f8264265a6e68b364af03f9a454048e55b7f
Caitlin Herbert <caitlinlherbert@gmail.com>
2025-01-09 21:09:44 +0000

    downloaded summernote

--- comlink/settings.py
+++ comlink/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
     'feed',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
--- comlink/urls.py
+++ comlink/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('feed.urls'), name='home'),
+    path('summernote/', include('django_summernote.urls')),
 ]
