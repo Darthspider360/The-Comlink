2d83a24ac507dadb27b6f9bd4cb8feac96aa08d1
Caitlin Herbert <caitlinlherbert@gmail.com>
2025-01-09 12:02:41 +0000

    bug fixed - copy paste error

--- comlink/settings.py
+++ comlink/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
     'feed',
@@ -51,7 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
